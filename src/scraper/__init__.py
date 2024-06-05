f1b75f06103da8ff61a1d828ff4be8841f6325d4
Cees <ceesvanspaendonck@gmail.com>
2024-06-05 22:24:16 +0200

    Updated module inits

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,1 +1,1 @@
-from .scraper import get_tweets_from_threads, scrape_threads, get_threads_by_search
+from .scraper import extract_tweets_from_url, get_tweets_from_threads, scrape_threads, get_threads_by_search
